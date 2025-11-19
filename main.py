2626e49cad2a7c124d9d930a121284998b7ff7cb
finny <phineas.ehrlich2026@gbwl.org>
2025-11-19 14:41:36 +0000

    Added scatterplot

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("hello world")
+import pandas as pd
+from matplotlib import pyplot as plt
 
+plt.style.use('seaborn-v0_8-deep')
+print(plt.style.available)
 
-if __name__ == "__main__":
-    main()
+# read csv
+df = pd.read_csv('class_data_v1.csv')
+print(df.info())
+
+# plotting
+hairLength = df['Hair Length Inches']
+heightInches = df['Height Inches']
+
+plt.scatter(hairLength, heightInches, cmap='seismic')
+plt.xlabel('Hair Length (Inches)')
+plt.ylabel('Height (Inches)')
+plt.savefig('hair_to_height.png')
+plt.close()
